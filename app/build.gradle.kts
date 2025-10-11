53a80d2fb31d6dbbf9bca6aca44d2ae1e7f090ac
Gustavo Augustini <63506279+gaugustini@users.noreply.github.com>
2025-10-11 15:28:34 -0300

    Update app version (0.2.1)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.gaugustini.mhfudatabase"
         minSdk = 23
         targetSdk = 36
-        versionCode = 4
-        versionName = "0.2.0"
+        versionCode = 5
+        versionName = "0.2.1"
     }
     buildTypes {
         release {
