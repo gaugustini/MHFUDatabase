ecaa68aac42fba3c44273e5ff0eec7d0eb8b164d
Gustavo Augustini <63506279+gaugustini@users.noreply.github.com>
2025-10-16 21:57:11 -0300

    Update dependencies
    - Compose:
        - `compose-bom`: '2025.09.00' -> '2025.10.00'
        - `material3`: '1.3.2' -> '1.4.0'
        - added `material-icons-core` (removed from material3:1.4.0)
        - `ui`: '1.9.1' -> '1.9.3'
        - `navigation-compose`: '2.9.4' -> '2.9.5'
    - Room: '2.8.0' -> '2.8.2'
    - Hilt: '2.57.1' -> '2.57.2'

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,22 +56,23 @@ dependencies {
     implementation("androidx.datastore:datastore-preferences:1.1.7")
 
     // Compose
-    implementation(platform("androidx.compose:compose-bom:2025.09.00"))
-    implementation("androidx.compose.material3:material3:1.3.2")
+    implementation(platform("androidx.compose:compose-bom:2025.10.00"))
+    implementation("androidx.compose.material3:material3:1.4.0")
+    implementation("androidx.compose.material:material-icons-core:1.7.8")
+    implementation("androidx.compose.ui:ui:1.9.3")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.9.3")
+    debugImplementation("androidx.compose.ui:ui-tooling:1.9.3")
     implementation("androidx.activity:activity-compose:1.11.0")
-    implementation("androidx.navigation:navigation-compose:2.9.4")
-    implementation("androidx.compose.ui:ui:1.9.1")
-    implementation("androidx.compose.ui:ui-tooling-preview:1.9.1")
-    debugImplementation("androidx.compose.ui:ui-tooling:1.9.1")
+    implementation("androidx.navigation:navigation-compose:2.9.5")
+    implementation("androidx.hilt:hilt-navigation-compose:1.3.0")
 
     // Room
-    implementation("androidx.room:room-runtime:2.8.0")
-    implementation("androidx.room:room-ktx:2.8.0")
-    ksp("androidx.room:room-compiler:2.8.0")
+    implementation("androidx.room:room-runtime:2.8.2")
+    implementation("androidx.room:room-ktx:2.8.2")
+    ksp("androidx.room:room-compiler:2.8.2")
 
     // Hilt
-    implementation("com.google.dagger:hilt-android:2.57.1")
-    implementation("androidx.hilt:hilt-navigation-compose:1.3.0")
-    ksp("com.google.dagger:hilt-android-compiler:2.57.1")
+    implementation("com.google.dagger:hilt-android:2.57.2")
+    ksp("com.google.dagger:hilt-android-compiler:2.57.2")
 
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -4,5 +4,5 @@ plugins {
     id("org.jetbrains.kotlin.android") version "2.2.20" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.2.20" apply false
     id("com.google.devtools.ksp") version "2.2.20-2.0.2" apply false
-    id("com.google.dagger.hilt.android") version "2.57.1" apply false
+    id("com.google.dagger.hilt.android") version "2.57.2" apply false
 }
