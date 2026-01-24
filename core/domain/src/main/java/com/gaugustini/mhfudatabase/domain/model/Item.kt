1cda53979ea6d0c4c4249e88cabaf2f75407b557
Gustavo Augustini <63506279+gaugustini@users.noreply.github.com>
2026-01-24 11:53:04 -0300

    Consolidate domain models for Items and Monsters
    - Merge `ItemSources.kt` and `ItemUsages.kt` into `Item.kt` to centralize item-related data structures.
    - Merge `MonsterAilmentStats.kt`, `MonsterDamageStats.kt`, and `MonsterItemEffectiveness.kt` into `Monster.kt` to centralize monster-related data structures.

--- core/domain/src/main/java/com/gaugustini/mhfudatabase/domain/model/Item.kt
+++ core/domain/src/main/java/com/gaugustini/mhfudatabase/domain/model/Item.kt
@@ -1,7 +1,9 @@
 package com.gaugustini.mhfudatabase.domain.model
 
+import com.gaugustini.mhfudatabase.domain.enums.GatherType
 import com.gaugustini.mhfudatabase.domain.enums.ItemIconColor
 import com.gaugustini.mhfudatabase.domain.enums.ItemIconType
+import com.gaugustini.mhfudatabase.domain.enums.Rank
 
 /**
  * Represents an item in the game.
@@ -38,4 +40,77 @@ data class Item(
 data class ItemQuantity(
     val item: Item,
     val quantity: Int,
+)
+
+/**
+ * Represents a source of an item.
+ */
+sealed interface ItemSource
+
+/**
+ * Represents a location where an item can be obtained.
+ *
+ * @property location The location where the item can be obtained.
+ * @property rank The rank where it is possible to obtain the item.
+ * @property type The gathering type to obtain the item (e.g., Gather, Mining,...).
+ * @property area The area number of the location.
+ */
+data class GatheringSource(
+    val location: Location,
+    val rank: Rank,
+    val type: GatherType,
+    val area: Int,
+) : ItemSource
+
+/**
+ * Represents a monster reward or drop item.
+ *
+ * @property monster The monster that drops the item.
+ * @property condition The condition required to receive the reward (e.g., "Break Wings").
+ * @property rank The rank of the monster required to receive the reward.
+ * @property stackSize The number of items in a stack reward.
+ * @property percentage The percentage chance of receiving the reward.
+ */
+data class MonsterSource(
+    val monster: Monster,
+    val condition: String,
+    val rank: Rank,
+    val stackSize: Int,
+    val percentage: Int?,
+) : ItemSource
+
+/**
+ * Represents all the ways a specific item can be obtained.
+ *
+ * @property combinations The list of item combinations where the result is the item.
+ * @property locations The list of locations where the item can be obtained.
+ * @property monsterRewards The list of monsters that drop the item.
+ */
+data class ItemSources(
+    val combinations: List<ItemCombination>,
+    val locations: List<GatheringSource>,
+    val monsterRewards: List<MonsterSource>,
+)
+
+/**
+ * Represents a usage of an item in the game.
+ */
+data class Usage<out T>(
+    val craftable: T,
+    val quantity: Int,
+)
+
+/**
+ * Represents all the ways a specific item can be utilized.
+ *
+ * @property combinations The list of item combinations that require the item to be crafted.
+ * @property armors The list of armors that require the item to be crafted.
+ * @property decorations The list of decorations that require the item to be crafted.
+ * @property weapons The list of weapons that require the item to be crafted.
+ */
+data class ItemUsages(
+    val combinations: List<ItemCombination>,
+    val armors: List<Usage<Armor>>,
+    val decorations: List<Usage<Decoration>>,
+    val weapons: List<Usage<Weapon>>,
 )
--- core/domain/src/main/java/com/gaugustini/mhfudatabase/domain/model/Monster.kt
+++ core/domain/src/main/java/com/gaugustini/mhfudatabase/domain/model/Monster.kt
@@ -1,5 +1,7 @@
 package com.gaugustini.mhfudatabase.domain.model
 
+import com.gaugustini.mhfudatabase.domain.enums.MonsterAilment
+import com.gaugustini.mhfudatabase.domain.enums.MonsterState
 import com.gaugustini.mhfudatabase.domain.enums.MonsterType
 import com.gaugustini.mhfudatabase.domain.enums.Rank
 
@@ -39,7 +41,84 @@ data class Monster(
 )
 
 /**
+ * Represents a monster's hitzone, detailing its weaknesses to different damage types.
+ *
+ * @property monsterId The unique identifier of the monster.
+ * @property name The name of the hitzone (e.g., "Head", "Tail", "Wings").
+ * @property cut The weakness to cutting damage.
+ * @property impact The weakness to impact damage.
+ * @property shot The weakness to shot damage.
+ * @property fire The weakness to fire damage.
+ * @property water The weakness to water damage.
+ * @property thunder The weakness to thunder damage.
+ * @property ice The weakness to ice damage.
+ * @property dragon The weakness to dragon damage.
+ */
+data class MonsterDamageStats(
+    val monsterId: Int,
+    val name: String,
+    val cut: Int,
+    val impact: Int,
+    val shot: Int,
+    val fire: Int,
+    val water: Int,
+    val thunder: Int,
+    val ice: Int,
+    val dragon: Int,
+)
+
+/**
+ * Represents a monster's resistance and tolerance to a specific ailment.
+ *
+ * @property monsterId The unique identifier of the monster.
+ * @property type The type of ailment (e.g., Poison, Paralysis, Sleep).
+ * @property initial The initial tolerance the monster has to the ailment.
+ * @property increase The amount the tolerance increases each time the ailment is applied.
+ * @property max The maximum tolerance the monster can have for the ailment.
+ * @property duration The duration of the ailment in seconds.
+ * @property damage The damage dealt by the ailment (if applicable).
+ */
+data class MonsterAilmentStats(
+    val monsterId: Int,
+    val type: MonsterAilment,
+    val initial: Int,
+    val increase: Int,
+    val max: Int,
+    val duration: Int,
+    val damage: Int,
+)
+
+/**
+ * Represents the effectiveness of certain items on a monster in different states.
+ *
+ * @property monsterId The unique identifier of the monster.
+ * @property monsterState The state of the monster (e.g., Normal, Enraged).
+ * @property canUsePitfallTrap Whether a pitfall trap can be used.
+ * @property canUseShockTrap Whether a shock trap can be used.
+ * @property canUseFlashBomb Whether a flash bomb can be used.
+ * @property canUseSonicBomb Whether a sonic bomb can be used.
+ * @property canUseDungBomb Whether a dung bomb can be used.
+ * @property canUseMeat Whether meat can be used.
+ */
+data class MonsterItemEffectiveness(
+    val monsterId: Int,
+    val monsterState: MonsterState,
+    val canUsePitfallTrap: Boolean,
+    val canUseShockTrap: Boolean,
+    val canUseFlashBomb: Boolean,
+    val canUseSonicBomb: Boolean,
+    val canUseDungBomb: Boolean,
+    val canUseMeat: Boolean,
+)
+
+/**
  * Represents a monster reward and its corresponding conditions.
+ *
+ * @property item The item rewarded.
+ * @property condition The condition required to receive the reward (e.g., "Break Wings").
+ * @property rank The rank of the monster required to receive the reward.
+ * @property stackSize The number of items in a stack reward.
+ * @property percentage The percentage chance of receiving the reward.
  */
 data class MonsterReward(
     val item: Item,
