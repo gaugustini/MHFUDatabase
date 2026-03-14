6467e00ed97613aab09aff4fe44e331e1cde7ff5
Gustavo Augustini <63506279+gaugustini@users.noreply.github.com>
2026-03-13 21:29:20 -0300

    finalize version 1.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.gaugustini.mhfudatabase"
         minSdk = 23
         targetSdk = 36
-        versionCode = 10
-        versionName = "1.0.0-rc02"
+        versionCode = 11
+        versionName = "1.0.0"
     }
     buildTypes {
         release {
