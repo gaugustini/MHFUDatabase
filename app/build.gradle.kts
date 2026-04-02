02be33a50966b9fd8cc1a43d6d0c044b43b0a807
Gustavo Augustini <63506279+gaugustini@users.noreply.github.com>
2026-04-02 20:26:30 -0300

    Bump version to 1.0.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.gaugustini.mhfudatabase"
         minSdk = 23
         targetSdk = 36
-        versionCode = 12
-        versionName = "1.0.1"
+        versionCode = 13
+        versionName = "1.0.2"
     }
     buildTypes {
         release {
