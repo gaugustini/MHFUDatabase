4dca677132ff360e5b6300ade72e52332252cf6b
Gustavo Augustini <63506279+gaugustini@users.noreply.github.com>
2026-07-31 19:20:31 -0300

    Update monster item effectiveness UI
    - Replace with a layout displaying all items.
    - Use `CheckCircle` and `Cancel` icons to indicate effectiveness for each item type.
    - Remove monster state text and "None" fallback text from the row.

--- app/src/main/java/com/gaugustini/mhfudatabase/ui/features/monster/components/MonsterItemEffectivenessListItem.kt
+++ app/src/main/java/com/gaugustini/mhfudatabase/ui/features/monster/components/MonsterItemEffectivenessListItem.kt
@@ -3,22 +3,26 @@ package com.gaugustini.mhfudatabase.ui.features.monster.components
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Cancel
+import androidx.compose.material.icons.filled.CheckCircle
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.BlendMode
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import com.gaugustini.mhfudatabase.R
 import com.gaugustini.mhfudatabase.domain.enums.ItemIconColor
-import com.gaugustini.mhfudatabase.domain.enums.MonsterState
 import com.gaugustini.mhfudatabase.domain.model.MonsterItemEffectiveness
 import com.gaugustini.mhfudatabase.ui.theme.Dimension
 import com.gaugustini.mhfudatabase.ui.theme.Theme
@@ -31,113 +35,55 @@ fun MonsterItemEffectivenessListItem(
     item: MonsterItemEffectiveness,
     modifier: Modifier = Modifier,
 ) {
+    val items = listOf(
+        item.canUseFlashBomb to (R.drawable.ic_item_ball to ItemIconColor.YELLOW),
+        item.canUseSonicBomb to (R.drawable.ic_item_ball to ItemIconColor.GRAY),
+        item.canUseShockTrap to (R.drawable.ic_item_trap to ItemIconColor.PURPLE),
+        item.canUsePitfallTrap to (R.drawable.ic_item_trap to ItemIconColor.GREEN),
+        item.canUseMeat to (R.drawable.ic_item_meat to ItemIconColor.RED),
+        item.canUseDungBomb to (R.drawable.ic_item_dung to ItemIconColor.YELLOW),
+    )
+
     Row(
         horizontalArrangement = Arrangement.spacedBy(Dimension.Spacing.medium),
         verticalAlignment = Alignment.CenterVertically,
         modifier = modifier
             .fillMaxWidth()
             .background(MaterialTheme.colorScheme.surface)
-            .padding(
-                horizontal = Dimension.Padding.large,
-                vertical = Dimension.Padding.medium
-            )
+            .padding(Dimension.Padding.large)
     ) {
-        Text(
-            text = stringResource(
-                when (item.monsterState) {
-                    MonsterState.NORMAL -> R.string.monster_state_normal
-                    MonsterState.ENRAGED -> R.string.monster_state_enraged
-                    MonsterState.UNSEEN -> R.string.monster_state_unseen
+        items.forEach { (isEffective, res) ->
+            val (iconRes, iconColor) = res
+
+            Box(
+                contentAlignment = Alignment.Center,
+                modifier = Modifier.weight(1f)
+            ) {
+                Box {
+                    Image(
+                        painter = painterResource(iconRes),
+                        contentDescription = null,
+                        colorFilter = ColorFilter.tint(
+                            MHFUColors.getItemColor(iconColor),
+                            BlendMode.Modulate
+                        ),
+                        modifier = Modifier
+                            .size(Dimension.Size.medium)
+                            .align(Alignment.Center),
+                        alpha = if (isEffective) 1f else 0.3f
+                    )
+                    Icon(
+                        imageVector =
+                            if (isEffective) Icons.Default.CheckCircle else Icons.Default.Cancel,
+                        contentDescription = null,
+                        modifier = Modifier
+                            .size(Dimension.Size.tiny)
+                            .background(MaterialTheme.colorScheme.surface, CircleShape)
+                            .align(Alignment.BottomEnd),
+                        tint = if (isEffective) Color(0xFF30C030) else Color(0xFFC03030)
+                    )
                 }
-            ),
-            style = MaterialTheme.typography.bodyMedium,
-            color = MaterialTheme.colorScheme.onSurface,
-            modifier = Modifier.weight(1f)
-        )
-
-        if (item.canUsePitfallTrap) {
-            Image(
-                painter = painterResource(R.drawable.ic_item_trap),
-                contentDescription = null,
-                colorFilter = ColorFilter.tint(
-                    color = MHFUColors.getItemColor(ItemIconColor.GREEN),
-                    blendMode = BlendMode.Modulate
-                ),
-                modifier = Modifier.size(Dimension.Size.small)
-            )
-        }
-        if (item.canUseShockTrap) {
-            Image(
-                painter = painterResource(R.drawable.ic_item_trap),
-                contentDescription = null,
-                colorFilter = ColorFilter.tint(
-                    color = MHFUColors.getItemColor(ItemIconColor.PURPLE),
-                    blendMode = BlendMode.Modulate
-                ),
-                modifier = Modifier.size(Dimension.Size.small)
-            )
-        }
-        if (item.canUseFlashBomb) {
-            Image(
-                painter = painterResource(R.drawable.ic_item_ball),
-                contentDescription = null,
-                colorFilter = ColorFilter.tint(
-                    color = MHFUColors.getItemColor(ItemIconColor.YELLOW),
-                    blendMode = BlendMode.Modulate
-                ),
-                modifier = Modifier.size(Dimension.Size.small)
-            )
-        }
-
-        if (item.canUseSonicBomb) {
-            Image(
-                painter = painterResource(R.drawable.ic_item_ball),
-                contentDescription = null,
-                colorFilter = ColorFilter.tint(
-                    color = MHFUColors.getItemColor(ItemIconColor.GRAY),
-                    blendMode = BlendMode.Modulate
-                ),
-                modifier = Modifier.size(Dimension.Size.small)
-            )
-        }
-
-        if (item.canUseDungBomb) {
-            Image(
-                painter = painterResource(R.drawable.ic_item_dung),
-                contentDescription = null,
-                colorFilter = ColorFilter.tint(
-                    color = MHFUColors.getItemColor(ItemIconColor.YELLOW),
-                    blendMode = BlendMode.Modulate
-                ),
-                modifier = Modifier.size(Dimension.Size.small)
-            )
-        }
-
-        if (item.canUseMeat) {
-            Image(
-                painter = painterResource(R.drawable.ic_item_meat),
-                contentDescription = null,
-                colorFilter = ColorFilter.tint(
-                    color = MHFUColors.getItemColor(ItemIconColor.RED),
-                    blendMode = BlendMode.Modulate
-                ),
-                modifier = Modifier.size(Dimension.Size.small)
-            )
-        }
-
-        if (
-            !item.canUsePitfallTrap &&
-            !item.canUseShockTrap &&
-            !item.canUseFlashBomb &&
-            !item.canUseSonicBomb &&
-            !item.canUseDungBomb &&
-            !item.canUseMeat
-        ) {
-            Text(
-                text = stringResource(R.string.monster_item_effectiveness_none),
-                style = MaterialTheme.typography.bodyMedium,
-                color = MaterialTheme.colorScheme.onSurface,
-            )
+            }
         }
     }
 }
