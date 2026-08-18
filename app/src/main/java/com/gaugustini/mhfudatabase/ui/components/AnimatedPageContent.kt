d3c06871f3546ff8349602ae5400072be6fae737
Gustavo Augustini <63506279+gaugustini@users.noreply.github.com>
2026-08-18 19:39:02 -0300

    Update `AnimatedPageContent` UI component
    - Wrap the page content in a `Surface` to apply the background color.

--- app/src/main/java/com/gaugustini/mhfudatabase/ui/components/AnimatedPageContent.kt
+++ app/src/main/java/com/gaugustini/mhfudatabase/ui/components/AnimatedPageContent.kt
@@ -8,6 +8,9 @@ import androidx.compose.animation.core.EaseIn
 import androidx.compose.animation.core.EaseOut
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.togetherWith
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 
@@ -50,6 +53,11 @@ fun <T> AnimatedPageContent(
         },
         modifier = modifier,
     ) { state ->
-        content(state)
+        Surface(
+            color = MaterialTheme.colorScheme.background,
+            modifier = Modifier.fillMaxSize()
+        ) {
+            content(state)
+        }
     }
 }
